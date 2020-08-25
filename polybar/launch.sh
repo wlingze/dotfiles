42b474ae8028f94b910d917ec1dd6448acd223c2
wlz <wl200103124@163.com>
2020-08-25 23:21:32 +0800

    polybar upup!

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q polybar
+killall polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar top
-
+# Launch polybar
+polybar main -c /home/$USER/.config/polybar/config.ini &
